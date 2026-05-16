68d81bbf2c044c0390c19be9de8b6f5f624c5eba
Trong Dinh <trongdth@gmail.com>
2026-05-16 17:11:05 +0700

    fix config on test suite

--- vite.screenshots.config.ts
+++ vite.screenshots.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import pkg from "./package.json";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
   resolve: {
     alias: {
       "@tauri-apps/plugin-store": path.resolve(__dirname, "src/mocks/store.ts"),
